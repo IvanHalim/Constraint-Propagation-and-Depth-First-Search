a57e1bc0294350f1170fef89f072958a1950fa17
ivanhalim <ivanhalim@users.noreply.github.com>
2018-11-12 01:17:13 -0800

    Update cross.cpp

---
+++ cross.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+string concatenate(char a, char b) {
+    return (string)a + (string)b;
+}
+
+vector<string> cross(string A, string B) {
+    vector<string> str;
+    for (int i = 0; i < A.length(); i++) {
+        for (int j = 0; j < B.length(); j++) {
+            str.push_back(concatenate(A[i],B[i]));
+        }
+    }
+
+    return str;
+}
+
+int main() {
+    vector<string> x = cross("ABC", "123");
+    for (int i = 0; i < x.size(); i++) {
+        cout << x[i] << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
