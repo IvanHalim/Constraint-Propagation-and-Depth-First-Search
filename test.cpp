60ae3ca90be7905c9fe0e8b03bc76005472da63e
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-12 18:51:46 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ using std::cin;
 
 int main() {
     sudoku mySudoku;
-    string grid1 = "530070000600195000098000060800060003400803001700020006060000280000419005000080079";
+    string grid1 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
     mySudoku.solve(grid1);
     mySudoku.display_solution();
 
