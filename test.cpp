0ecad63f8ff511f14758452c329f8f2491eb7a29
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-12 22:51:47 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,11 +2,15 @@
 #include "sudoku.hpp"
 
 using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
     sudoku mySudoku;
     string grid1 = "800000000003600000070090200050007000000045700000100030001000068008500010090000400";
     mySudoku.solve(grid1);
+    mySudoku.display_input();
+    cout << endl;
     mySudoku.display_solution();
 
     return 0;
