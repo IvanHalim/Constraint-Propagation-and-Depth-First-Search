e862d67a5aed750c7e9085c4cf758e5f0e8647dc
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-13 17:35:03 -0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -8,9 +8,12 @@ using std::endl;
 int main() {
     sudoku mySudoku;
     string grid1 = "800000000003600000070090200050007000000045700000100030001000068008500010090000400";
-    mySudoku.solve(grid1);
+    mySudoku.parse_grid(grid1);
     mySudoku.display_input();
     cout << endl;
+    mySudoku.display_solution();
+    cout << endl;
+    mySudoku.solve(grid1);
     mySudoku.display_solution();
     cout << endl;
     grid1 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
@@ -20,8 +23,6 @@ int main() {
     mySudoku.display_solution();
     cout << endl;
     mySudoku.solve(grid1);
-    mySudoku.display_input();
-    cout << endl;
     mySudoku.display_solution();
     cout << endl;
 
