eab222ed5f12b9339291893d7450280b161865e5
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-26 00:50:22 -0800

    Add Rust translation

--- rust-translation/src/main.rs
+++ rust-translation/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::{HashMap};
+
+
+#[derive(Debug)]
+struct Sudoku {
+    cols:       Vec<char>,
+    rows:       Vec<char>,
+    squares:    Vec<String>,
+    unitlist:   Vec<Vec<String>>,
+    units:      HashMap<String, Vec<Vec<String>>>,
+    peers:      HashMap<String, Vec<String>>
 }
+
+fn cross(A: &[char], B: &[char]) -> Vec<String> {
+    let mut cross = Vec::new();
+    for a in A {
+        for b in B {
+            let mut sq = String::new();
+            sq.push(*a);
+            sq.push(*b);
+            cross.push(sq)
+        }
+    }
+
+    cross
+}
+
