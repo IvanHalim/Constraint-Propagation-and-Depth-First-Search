6e83e3bb0b39801a4f721a8b9d37e276af664edf
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-27 18:46:14 -0800

    Test for diabolical puzzle

--- cpp-translation/test.cpp
+++ cpp-translation/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include "sudoku.hpp"
 
 using std::cin;
@@ -11,6 +12,17 @@ int main() {
     mySudoku.solve_all("../input/easy50.txt", "easy");
     mySudoku.solve_all("../input/top95.txt", "hard");
     mySudoku.solve_all("../input/hardest.txt", "hardest");
+    cout << endl;
+
+    string diabolical  = ".....6....59.....82....8....45........3........6..3.54...325..6..................";
+    auto t1 = std::chrono::high_resolution_clock::now();
+    mySudoku.solve(diabolical);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = t2 - t1;
+
+    mySudoku.display_input();
+    mySudoku.display_solution();
+    cout << elapsed.count() << " secs" << endl << endl;
 
     system("pause");
     return 0;
