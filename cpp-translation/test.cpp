115a5fcb0264794fd628b625c882b66450a0228c
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-27 18:23:51 -0800

    Update

--- cpp-translation/test.cpp
+++ cpp-translation/test.cpp
@@ -8,9 +8,9 @@ using std::endl;
 int main() {
     sudoku mySudoku;
     mySudoku.test();
-    mySudoku.solve_all("easy50.txt", "easy");
-    mySudoku.solve_all("top95.txt", "hard");
-    mySudoku.solve_all("hardest.txt", "hardest");
+    mySudoku.solve_all("../input/easy50.txt", "easy");
+    mySudoku.solve_all("../input/top95.txt", "hard");
+    mySudoku.solve_all("../input/hardest.txt", "hardest");
 
     system("pause");
     return 0;
