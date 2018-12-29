ea01ef8f8c3ce0a58ca5dc59f66cf1db80c5491f
ivanhalim <ivanhalim@users.noreply.github.com>
2018-12-29 07:09:22 -0800

    Update

--- cpp-translation/sudoku.cpp
+++ cpp-translation/sudoku.cpp
@@ -61,7 +61,7 @@ bool string_contains(const string &str, const string &substring) {
  * A helper function to erase a substring from a string
  */
 string string_eliminate(string str, const string &substring) {
-    int position = str.find(substring);
+    size_t position = str.find(substring);
     if (position == string::npos) {
         return str;
     } else {
@@ -72,9 +72,9 @@ string string_eliminate(string str, const string &substring) {
 /*
  * A helper function to center a string inside a given width
  */
-string center_string(string str, int width) {
-    int leading_spaces = (width - str.length()) / 2;
-    int trailing_spaces = width - str.length() - leading_spaces;
+string center_string(string str, size_t width) {
+    size_t leading_spaces = (width - str.length()) / 2;
+    size_t trailing_spaces = width - str.length() - leading_spaces;
     str.insert(0, leading_spaces, ' ');
     str.append(trailing_spaces, ' ');
     return str;
@@ -83,8 +83,8 @@ string center_string(string str, int width) {
 /*
  * A helper function to find the maximum length of the values in the grid
  */
-int find_max_length(const map<string, string> &values) {
-    int max_length = 0;
+size_t find_max_length(const map<string, string> &values) {
+    size_t max_length = 0;
     for (const auto &i : values) {
         if ((i.second).length() > max_length) {
             max_length = (i.second).length();
@@ -97,7 +97,7 @@ int find_max_length(const map<string, string> &values) {
  * A helper function to find the square with the minimum number of possibilities
  */
 string find_min_possibilities(const map<string, string> &values) {
-    int min_length = 10;
+    size_t min_length = 10;
     string min_square;
     for (const auto &i : values) {
         if ((i.second).length() < min_length && (i.second).length() > 1) {
@@ -124,10 +124,10 @@ bool solved(const map<string, string> &values) {
  * A helper function to create a horizontal line on a sudoku grid
  * given a single column width
  */
-string create_line(int width, int size) {
+string create_line(size_t width, size_t size) {
     string line;
-    for (int i = 0; i < size*size; ++i) {
-        for (int j = 0; j < width; ++j) {
+    for (size_t i = 0; i < size*size; ++i) {
+        for (size_t j = 0; j < width; ++j) {
             line += '-';
         }
         if ((i + 1) % size == 0 && (i + 1) < size*size) {
@@ -155,10 +155,10 @@ void sudoku::display(map<string, string> &values) {
         cout << "**NO SOLUTION IS FOUND**\n" << endl;
         return;
     }
-    int width = 1 + find_max_length(values);
+    size_t width = 1 + find_max_length(values);
     string line = create_line(width, size);
-    for (int i = 0; i < rows.length(); ++i) {
-        for (int j = 0; j < cols.length(); ++j) {
+    for (size_t i = 0; i < rows.length(); ++i) {
+        for (size_t j = 0; j < cols.length(); ++j) {
             cout << center_string(values[squares[i * size*size + j]], width);
             if ((j + 1) % size == 0 && (j + 1) < size*size) {
                 cout << '|';
@@ -184,7 +184,7 @@ void sudoku::display(map<string, string> &values) {
  */
 map<string, string> sudoku::grid_values(const string &grid) {
     vector<string> chars;
-    for (int i = 0; i < grid.length(); ++i) {
+    for (size_t i = 0; i < grid.length(); ++i) {
         if (string_contains(digits, grid.substr(i, 1))) {
             chars.push_back(grid.substr(i, 1));
         } else if (string_contains("0.", grid.substr(i, 1))) {
@@ -193,7 +193,7 @@ map<string, string> sudoku::grid_values(const string &grid) {
     }
     assert(chars.size() == squares.size());
     map<string, string> grid_val;
-    for (int i = 0; i < squares.size(); ++i) {
+    for (size_t i = 0; i < squares.size(); ++i) {
         grid_val[squares[i]] = chars[i];
     }
     return grid_val;
@@ -216,7 +216,7 @@ sudoku::sudoku() {
      */
     vector<string> rows_threes;
     vector<string> cols_threes;
-    for (int i = 0; i < size*size; i += size) {
+    for (size_t i = 0; i < size*size; i += size) {
         rows_threes.push_back(rows.substr(i, size));
         cols_threes.push_back(cols.substr(i, size));
     }
@@ -228,10 +228,10 @@ sudoku::sudoku() {
      *             [cross(r, cols) for r in rows] +
      *             [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
      */
-    for (int i = 0; i < rows.length(); ++i) {
+    for (size_t i = 0; i < rows.length(); ++i) {
         unitlist.push_back(cross(rows.substr(i, 1), digits));
     }
-    for (int i = 0; i < digits.length(); ++i) {
+    for (size_t i = 0; i < digits.length(); ++i) {
         unitlist.push_back(cross(rows, digits.substr(i, 1)));
     }
     for (const auto &rs : rows_threes) {
@@ -347,7 +347,7 @@ bool sudoku::parse_grid(const string &grid) {
  */
 bool sudoku::assign(map<string, string> &values, const string &s, const string &d) {
     string other_values = string_eliminate(values[s], d);
-    for (int i = 0; i < other_values.length(); ++i) {
+    for (size_t i = 0; i < other_values.length(); ++i) {
         if (!eliminate(values, s, other_values.substr(i, 1))) {
             return false;
         }
@@ -441,7 +441,7 @@ bool sudoku::search(map<string, string> &values) {
     }
     string s = find_min_possibilities(values);
     map<string, string> copy = values;
-    for (int i = 0; i < values[s].length(); ++i) {
+    for (size_t i = 0; i < values[s].length(); ++i) {
         if (assign(values, s, values[s].substr(i, 1))) {
             if (search(values)) {
                 return true;
@@ -483,8 +483,8 @@ void sudoku::test() {
 void sudoku::solve_all(const string &file_name, const string &name, double show_if) {
     string grid;
     vector<bool> results;
-    int N, frequency;
-    int sum_results = 0;
+    size_t N, frequency;
+    size_t sum_results = 0;
     double sum_time = 0;
     double max_time = 0;
     double avg_time;
@@ -545,7 +545,7 @@ bool sudoku::solved(map<string, string> &values) {
     }
 
     set<string> set_digits;
-    for (int i = 0; i < digits.length(); ++i) {
+    for (size_t i = 0; i < digits.length(); ++i) {
         set_digits.insert(digits.substr(i, 1));
     }
 
