b98e75e5e6b49475241ea6f40f51800a53e721a8
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-02 14:43:07 -0800

    Update main.rs

--- rust-translation/src/main.rs
+++ rust-translation/src/main.rs
@@ -12,9 +12,9 @@ struct context {
 
 fn cross(A: &[char], B: &[char]) -> Vec<String> {
     let mut cross : Vec<String> = Vec::new();
+    let mut sq = String::new();
     for a in A {
         for b in B {
-            let mut sq = String::new();
             sq.push(*a);
             sq.push(*b);
             cross.push(sq)
@@ -24,16 +24,27 @@ fn cross(A: &[char], B: &[char]) -> Vec<String> {
 }
 
 fn grid_values(grid: &str, ctx: &context) -> HashMap<String, Vec<char>> {
-    let mut chars : Vec<Vec<char>> = Vec::new();
-    for c in grid.chars() {
-
-    }
-    assert_eq!(chars.len(), 81);
+    let mut grid_chars : Vec<Vec<char>> =
+        grid.chars()
+        .filter(|c| ctx.cols.contains(c) || ['0', '1'].contains(c))
+        .map(|c| vec![c]).collect();
+    assert_eq!(grid_chars.len(), 81);
     let grid_val : HashMap<String, Vec<char>> =
-        ctx.squares.iter().cloned().zip(chars.into_iter()).collect();
+        ctx.squares.iter().cloned().zip(grid_chars.into_iter()).collect();
     grid_val
 }
 
-fn parse_grid(grid: &str, ctx: &context) -> HashMap<String, Vec<char>> {
+fn parse_grid(grid: &str, ctx: &context) -> Option<HashMap<String, Vec<char>>> {
     let mut values : HashMap<String, Vec<char>> = HashMap::new();
+    for s in &ctx.squares {
+        values.insert(s.clone(), ctx.cols.clone());
+    }
+    for (s, digits) in &grid_values(grid, ctx) {
+        for d in digits {
+            if ctx.cols.contains(d) && !assign(&mut values, s, d, ctx) {
+                return None;
+            }
+        }
+    }
+    Some(values)
 }
