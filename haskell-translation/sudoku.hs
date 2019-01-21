1b1f789ad65e3ad65df63a2e08c7de9aa6653a7e
ivanhalim <ivanhalim@users.noreply.github.com>
2019-01-21 12:28:46 -0800

    Update sudoku.hs

--- haskell-translation/sudoku.hs
+++ haskell-translation/sudoku.hs
@@ -37,18 +37,27 @@ set (Just x) = nub (concat x)
 peers :: Map Square [Square]
 peers = Map.fromList [(s, set (Map.lookup s units)) | s <- squares]
 
+regular :: String -> Bool
+regular grid = length (filter (`elem` "0.123456789") grid) == 81
+
+emptyGrid :: Grid
+emptyGrid = Map.fromList [(s, digits) | s <- squares]
+
 parse_grid :: String -> Maybe Grid
 parse_grid grid
-    | length (filter (`elem` "0.123456789") grid) == 81 = foldM assign emptyGrid (zip squares grid)
-    | otherwise                                         = Nothing
-    where
-        emptyGrid = Map.fromList [(s, digits) | s <- squares]
+    | regular grid = foldM assign emptyGrid (zip squares grid)
+    | otherwise    = Nothing
 
 assign :: Grid -> (Square, Digit) -> Maybe Grid
 assign g (s,d) = foldM (eliminate s) g (filter (/= d) (fromJust (Map.lookup s g)))
 
 eliminate :: Square -> Grid -> Digit -> Maybe Grid
 eliminate = undefined
+-- eliminate s g d
+--     | not elem d (fromJust (Map.lookup s g))                  = Just g
+--     | length (filter (/= d) (fromJust (Map.lookup s g))) == 0 = Nothing
+--     | length (filter (/= d) (fromJust (Map.lookup s g))) == 1 =
+
 
 main :: IO ()
 main = return ()
